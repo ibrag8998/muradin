ce3bf40886fb4b497da18d16ccfcbe0d24201f20
ibrag8998 <ibragdzh@gmail.com>
2021-06-21 12:17:06 +0300

    many products in one order

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 
-from orders.models import Order
+from orders.models import Order, OrderProduct
+
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
-    list_display = ['product', 'count', 'get_total_price', 'created_at', 'get_address']
+    list_display = ['get_products_str', 'get_total_count', 'get_total_price', 'created_at', 'get_address']
+    inlines = [OrderProductInline]
 
     @admin.display(description="Адрес")
     def get_address(self, obj):
         return obj.client.addresses.first()
+
+    @admin.display(description="Товары")
+    def get_products_str(self, obj):
+        if (order_product := obj.order_products.first()) is None:
+            return None
+        return f"{order_product.product.name}, ..."
--- orders/models.py
+++ orders/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Order(models.Model):
     client = models.ForeignKey('clients.Client', on_delete=models.SET_NULL, null=True, related_name='orders',
                                verbose_name="клиент")
-    product = models.ForeignKey('products.Product', on_delete=models.SET_NULL, null=True, related_name='orders',
-                                verbose_name="товар")
-    count = models.PositiveIntegerField("Количество товара", default=1)
+    # product = models.ForeignKey('products.Product', on_delete=models.SET_NULL, null=True, related_name='orders',
+    #                             verbose_name="товар")
+    # count = models.PositiveIntegerField("Количество товара", default=1)
     provider = models.ForeignKey('providers.Provider', on_delete=models.SET_NULL, null=True, related_name='orders',
                                  verbose_name="поставщик")
     created_at = models.DateTimeField("Дата и время заказа", auto_now_add=True)
@@ -19,6 +19,26 @@ class Order(models.Model):
         return f"Заказ #{self.id}"
 
     def get_total_price(self):
-        return self.product.retail_price * self.count
+        return sum(order_product.product.retail_price * order_product.product.count for order_product in
+                   self.order_products.all())
 
     get_total_price.short_description = "Сумма заказа"
+
+    def get_total_count(self):
+        return self.order_products.aggregate(total_count=models.Sum('count'))['total_count']
+
+    get_total_count.short_description = "Итого единиц товара"
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_products', verbose_name="Заказ")
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='order_products',
+                                verbose_name="Товар")
+    count = models.PositiveIntegerField("Количество товара", default=1)
+
+    class Meta:
+        verbose_name = "товар из заказа"
+        verbose_name_plural = "товары из заказа"
+
+    def __str__(self):
+        return f"{self.product.name} x{self.count} из заказа #{self.order.id}"
