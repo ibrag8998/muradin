8914cf6497dccfab7488f55960af15c1583cbebb
ibrag8998 <ibragdzh@gmail.com>
2021-06-21 11:59:30 +0300

    order created_at datetime

--- orders/admin.py
+++ orders/admin.py
@@ -5,7 +5,8 @@ from orders.models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['product', 'count', 'get_total_price', 'get_address']
+    readonly_fields = ['created_at']
+    list_display = ['product', 'count', 'get_total_price', 'created_at', 'get_address']
 
     @admin.display(description="Адрес")
     def get_address(self, obj):
--- orders/models.py
+++ orders/models.py
@@ -9,10 +9,14 @@ class Order(models.Model):
     count = models.PositiveIntegerField("Количество товара", default=1)
     provider = models.ForeignKey('providers.Provider', on_delete=models.SET_NULL, null=True, related_name='orders',
                                  verbose_name="поставщик")
+    created_at = models.DateTimeField("Дата и время заказа", auto_now_add=True)
 
     class Meta:
         verbose_name = "заказ"
         verbose_name_plural = "заказы"
+
+    def __str__(self):
+        return f"Заказ #{self.id}"
 
     def get_total_price(self):
         return self.product.retail_price * self.count
