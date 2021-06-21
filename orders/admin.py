870962db4b90728f4dcda793718e8a00eecfb2e9
ibrag8998 <ibragdzh@gmail.com>
2021-06-21 11:54:47 +0300

    add addresses field to orders and change readme

--- orders/admin.py
+++ orders/admin.py
@@ -5,4 +5,8 @@ from orders.models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['product', 'count', 'get_total_price']
+    list_display = ['product', 'count', 'get_total_price', 'get_address']
+
+    @admin.display(description="Адрес")
+    def get_address(self, obj):
+        return obj.client.addresses.first()
